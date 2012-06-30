009ca3c16824a7a9a88ca301ea1fecf41ea7db98
Alexandre Carlton <acar7163@uni.sydney.edu.au>
2012-06-30 22:42:42 +1000

    Added a basic Dijkstra algorithm, and allowed for the implementation of graphs containing isolated nodes by forcing the user to include a list of the vertices in the __init__ method as well as a list of edges between these vertices.

--- Adjacency_List.py
+++ Adjacency_List.py
@@ -1,50 +1,49 @@
 from collections import deque
 
-def edge_list(file):
-    '''Convert text file into a list of tuples'''
-    return [pair.strip().split() for pair in open(file, 'rU')]
+def vertices_and_edges(file):
+    '''Convert text file into a list of vertices and edges'''
+    vertices = [vertex.strip() for vertex in open(file, 'rU') if len(vertex.strip().split()) == 1]
+    edges = [pair.strip().split() for pair in open(file, 'rU') if len(pair.strip().split()) in (2,3)]
+    print vertices
+    return vertices, edges
 
+
+#PROBLEM: Doesn't allow for isolated vertices (only takes in connected graphs)
+#Resolution: Take in a list of vertices as well as a list of edges.
 class Adjacency_List(object):
     '''A dictionary where the key is a vertex and the value a list of all the vertices it is adjacent to.
     Includes algorithms such as BFS, DFS, etc.'''
 
-    def __init__(self, edge_list, directed=False, weighted=False):#edge_list being a list of tuples (eg. ('A', 'B', 2))
+    def __init__(self, vertices, edges, directed=False, weighted=False):#edge_list being a list of tuples (eg. ('A', 'B', 2))
         if not isinstance(directed, bool):
             raise TypeError("Directed must be a boolean value.")
         if not isinstance(weighted, bool):
             raise TypeError("Weighted must be a boolean value.")
-        
+               
         self.directed = directed
         self.weighted = weighted
         
-        self.adj_list = {}
-        self.edge_list = edge_list
+        self.adj_list = {vertex : [] for vertex in vertices}
+        self.edge_list = edges
         if self.weighted:
             self.weight = {} #weight[(a,b)] -> int
 
-        
+
         #Could probably make an add_edge(A,B) or something to make this easier.               
-        for edge in edge_list:
-            if self.weighted and len(edge) is not 3:
+        for edge in self.edge_list:
+            if self.weighted and len(edge) != 3:
                 raise ValueError("Only two vertices and an edge per tuple.")
-            if not self.weighted and len(edge) is not 2:
+            if not self.weighted and len(edge) != 2:
                 raise ValueError("Only two vertices per tuple.")
 
             v1, v2 = edge[:2]
-
-            if v1 in self.adj_list.keys():
-                self.adj_list[v1].append(v2)
-            else:
-                self.adj_list[v1] = [v2]
+            self.adj_list[v1].append(v2)
             if self.weighted:
                 self.weight[(v1,v2)] = int(edge[2])
             
             #v1 is adjacent to v2 as well if the graph is not directed
             if not self.directed:
-                if v2 in self.adj_list.keys():
-                    self.adj_list[v2].append(v1)
-                else:
-                    self.adj_list[v2] = [v1]
+                self.adj_list[v2].append(v1)
                 if self.weighted:
                     self.weight[(v2,v1)] = int(edge[2])
 
@@ -85,16 +84,18 @@ class Adjacency_List(object):
         return self.edge_list[:]
     
     def num_edges(self):
+        '''Returns the number of edges in the graph.'''
         return len(self.edge_list)
 
     def num_vertices(self):
+        '''Returns the number of vertices in the graph.'''
         return len(self.adj_list.keys())
 
-    def weight(a, b):
-        '''Returns the weight of a given edge if the graph is weighted.'''
+    def weight(v1, v2):
+        '''Returns the weight of an edge between two given vertices (if the graph is weighted).'''
         if not self.weighted:
             raise ValueError("Graph is not weighted.")
-        return self.weight[(a,b)]
+        return self.weight[(v1,v2)]
 
 
     ########################
@@ -141,26 +142,59 @@ class Adjacency_List(object):
         return self._XFS('DFS', root)
 
 
-    ################
-    # Minimum paths#
-    ###############
+    #################
+    # Minimum paths #
+    #################
 
     def Floyd_Warshall(self):
-        '''Returns a dictionary where each pair a,b returns the minimum path of each distance'''
+        '''Returns a dictionary where each pair (v1,v2) returns the length of the smallest path between them.'''
         if not self.weighted:
             raise ValueError("Graph must be weighted in order to execute algorithm.")
         
         vertices = self.adj_list.keys() 
-        path = dict([ ( (v1,v2), 0 if v1 is v2 
+        path = { (v1,v2): 0 if v1 is v2 
                        else self.weight[(v1,v2)] if (v1,v2) in self.weight.keys() 
-                       else float('inf')) 
+                       else float('inf') 
                      for v1 in vertices
-                     for v2 in vertices])
+                     for v2 in vertices}
     
         for k in vertices:
             for i in vertices:
                 for j in vertices:
-                    path[(i,j)] = min(path[(i,j)], path[(i,k)]+path[(k,j)])
+                    path[(i,j)] = min(path[(i,j)], path[(i,k)] + path[(k,j)])
 
         return path
+    
 
+    def Dijkstra(self, root=None, target=None):
+        ''' Description goes here '''
+        if not self.weighted:
+            raise ValueError('Graph must be weighted in order to execute algorithm.')
+        
+        dist = { vertex : float('inf') for vertex in self.adj_list.iterkeys() }
+        previous = { vertex : None for vertex in self.adj_list.iterkeys() }
+        vertices = self.adj_list.keys()
+
+        root = root or self.adj_list.keys()[0]
+        dist[root] = 0
+
+        while len(vertices):
+            vertex = min(vertices, key = lambda v: dist[v]) #Grabs key with smallest value
+###            if target == vertex: return dist
+            
+            if dist[vertex] == float('inf'):
+                break;
+
+            vertices.remove(vertex)
+
+            for adjacent_vertex in self.adj_list[vertex]:
+                alt = dist[vertex] + self.weight[(vertex, adjacent_vertex)]
+                if alt < dist[adjacent_vertex]:
+                    dist[adjacent_vertex] = alt
+                    previous[adjacent_vertex] = vertex
+
+                    #Shift adjacent_vertex left
+                    i = vertices.index(adjacent_vertex)
+                    vertices[i], vertices[i-1] = vertices[i-1], vertices[i]
+
+        return dist
